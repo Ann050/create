46215384fe4be82ee520ae85d16eeee555b7d3f0
Ann050 <62489640+Ann050@users.noreply.github.com>
2020-04-14 20:47:58 +0800

    Add files via upload

---
+++ 2020-03-10 sort.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        sort
+# Purpose:
+#
+# Author:      chenw_000
+#
+# Created:     10/03/2020
+# Copyright:   (c) chenw_000 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def insersion_sort(list):
+    for index in range(1,len(list)):
+        value = list[index]
+        i = index -1
+        while i >= 0:
+            if list[index] < list[i]:
+                list[i+1] = list[i]
+                list[i] = value
+            else:
+                break
+
+
+
